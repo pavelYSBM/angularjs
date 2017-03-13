a75f38c3579a1fb7755431de0f51d43c91067e0e
admin <pavlik111188@yandex.ru>
2017-03-13 16:41:23 +0200

    'test'

---
+++ js/controllers/mainController.js
@@ -0,0 +1,29 @@
+// create angular controller
+validationApp.controller('ExampleController', ['$scope', function($scope) {
+        $scope.master = {};
+        $scope.allUsers = [];
+
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+        };
+
+        $scope.add = function(user) {
+            $scope.allUsers.push(user);
+            $scope.user = {};
+        };
+
+        $scope.reset = function(form) {
+            if (form) {
+                form.$setPristine();
+                form.$setUntouched();
+            }
+            $scope.user = angular.copy($scope.master);
+        };
+
+        $scope.reset();
+    }])
+    .directive('allUsers', function() {
+        return {
+            template: 'All Users = {{user.name}}'
+        };
+    });
